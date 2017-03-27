4035c044e50e8ddc78d4e7cb02090bc12bedc468
yrq110 <yrq1025@gmail.com>
2017-03-27 20:07:21 +0800

    add js/css/img/html-package

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const webpack = require('webpack');
 const path = require('path');
 
 const webpackConfig = {
   entry :
-    { 
+    {
       app: path.resolve(__dirname, 'src/index')
     },
   output : {
@@ -13,22 +14,35 @@ const webpackConfig = {
   },
   module: {
     rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      options: {
-        presets: ['react', "es2015"],
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', "es2015"],
+        },
       },
-    },
-    {
-      test: /\.css$/,
-      use: ['style-loader','css-loader'],
-    }]
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+　　　　　　test: /\.(png|jpg)$/,
+　　　　　　use: 'url-loader?limit=8192&name=images/[name].[ext]'
+　　　 },
+      {
+　　　　　　test: /\.html$/,
+　　　　　　use: 'html-loader'
+　　　 }
+    ]
   },
   plugins: [
       new webpack.optimize.UglifyJsPlugin(),
-      new HtmlWebpackPlugin({template: 'src/index.html'})
+      new HtmlWebpackPlugin({template: 'src/index.html'}),
+      new ExtractTextPlugin("index.css"),
   ],
 };
 
